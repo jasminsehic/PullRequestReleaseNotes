0b5142fba994d891c6f1de7e552c7a046a8d3251
Jasmin Sehic <jasminsehic@gmail.com>
2019-06-18 22:14:20 +0800

    Upgraded csproj to new 2017 format and upgraded Fody

--- src/PullRequestReleaseNotes/Properties/AssemblyInfo.cs
+++ src/PullRequestReleaseNotes/Properties/AssemblyInfo.cs
@@ -1,14 +1,5 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("PullRequestReleaseNotes")]
-[assembly: AssemblyDescription("PullRequestReleaseNotes is a utility which generates release notes for all merged pull requests on a specific branch that have not yet been released (since last tag)")]
-[assembly: AssemblyCompany("Jasmin Sehic")]
-[assembly: AssemblyProduct("PullRequestReleaseNotes")]
-[assembly: AssemblyCopyright("Copyright © Jasmin Sehic 2016")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b7a7df45-4248-4784-8417-594f15b7484a")]
-
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
