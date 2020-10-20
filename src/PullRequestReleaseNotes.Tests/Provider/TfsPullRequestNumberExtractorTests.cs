10cf81249ee729a02704a4ae35d73a3b26d4df47
Eben Zhang <zhangyingneng@gmail.com>
2020-10-20 13:43:11 +0800

    Test: add test case for the TFS pull request number extration

---
+++ src/PullRequestReleaseNotes.Tests/Provider/TfsPullRequestNumberExtractorTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using PullRequestReleaseNotes.Providers;
+using Shouldly;
+
+namespace PullRequestReleaseNotes.Tests.Provider
+{
+    [TestFixture]
+    public class TfsPullRequestNumberExtractorTests
+    {
+        [TestCase("Merge pull request 123: fix a bug", 123)]
+        [TestCase("Merged PR 123: fix a bug", 123)]
+        [TestCase("t 123: number 123 was was extracted by the old code", null)]
+        public void CanExtractPullRequestNumber(string commit, int? expected)
+        {
+            TfsPullRequestNumberExtractor.Extract(commit).ShouldBe(expected);
+        }
+    }
+}
