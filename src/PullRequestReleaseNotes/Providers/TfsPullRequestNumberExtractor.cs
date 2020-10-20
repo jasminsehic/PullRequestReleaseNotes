48600d4d441f857b5239ad66bfd9b9bfed2e4437
Eben Zhang <zhangyingneng@gmail.com>
2020-10-20 14:11:12 +0800

    Fix: wrong commit picked up as pr merge commit
    because the previous code matches any single char int
    
    [Merge pull request|Merged PR]

--- src/PullRequestReleaseNotes/Providers/TfsPullRequestNumberExtractor.cs
+++ src/PullRequestReleaseNotes/Providers/TfsPullRequestNumberExtractor.cs
@@ -6,7 +6,7 @@ namespace PullRequestReleaseNotes.Providers
     {
         public static int? Extract(string commitMessage)
         {
-            var pattern = new Regex(@"[Merge pull request|Merged PR] (?<pullRequestNumber>\d+).*");
+            var pattern = new Regex(@"(Merge pull request|Merged PR) (?<pullRequestNumber>\d+).*");
             var match = pattern.Match(commitMessage);
             if (match.Groups.Count <= 0 || !match.Groups["pullRequestNumber"].Success)
                 return null;
